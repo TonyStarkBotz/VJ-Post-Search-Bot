4b1c4714a8d3cdd4ad91773f5d73e8cd9cc4cca0
Tony Stark <138078424+TonyStarkBotz@users.noreply.github.com>
2024-07-22 13:44:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @TonyStark_Botz
+# Join our Telegram Channel For Amazing Bot @TonyStark_Botz
+# Ask Doubt on telegram @TonyStarkBotzXSupport
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'TechVJ'
+    return 'TonyStark'
 
 
 if __name__ == "__main__":
