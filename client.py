e685c8c6480f15c1a57ba81e50266b47c782cc9a
Tony Stark <138078424+TonyStarkBotz@users.noreply.github.com>
2024-07-22 13:44:21 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @TonyStark_Botz
+# Join our Telegram Channel For Amazing Bot @TonyStark_Botz
+# Ask Doubt on telegram @TonyStarkBotzXSupport
 
 from info import *
 from pyrogram import Client
@@ -8,13 +8,13 @@ from pyrogram import Client
 class Bot(Client):   
     def __init__(self):
         super().__init__(   
-           "vj-post-search-bot",
+           "tony-post-search-bot",
             api_id=API_ID,
             api_hash=API_HASH,           
             bot_token=BOT_TOKEN,
             plugins={"root": "plugins"})
     async def start(self):                        
         await super().start()  
-        print("Bot Started 🔧 Powered By @VJ_Botz")   
+        print("Bot Started 🔧 Powered By @TonyStark_Botz")   
     async def stop(self, *args):
         await super().stop()
